1c12b8ba3ce5a0086cf97fadc4708dd537ef9394
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-27 20:33:09 -0300

    Creado layout

---
+++ src/layout/Layout.js
@@ -0,0 +1,11 @@
+import './Layout.css';
+
+function Layout() {
+    return (
+        <div className="layout">
+            <h3 style={{color:'red'}}>This is the layout</h3>
+            <p style={{'text-transform':'uppercase',color:'blue'}}>Hola, mundo!</p>
+        </div>
+    );
+}
+export default Layout;
