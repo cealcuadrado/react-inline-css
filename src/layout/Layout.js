5497708a125bc2336a2669d8a686be51c7cbbbdb
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-27 20:35:39 -0300

    Aplicado formato camelCase para propiedades con colon

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -4,7 +4,11 @@ function Layout() {
     return (
         <div className="layout">
             <h3 style={{color:'red'}}>This is the layout</h3>
-            <p style={{'text-transform':'uppercase',color:'blue'}}>Hola, mundo!</p>
+            <p style={{ textTransform: 'uppercase', color: 'blue' }}>Hola, mundo!</p>
+            
+            <div style={{borderRadius:'15px',border:'1px solid black'}}>
+                <p>Este es un elemento DIV</p>
+            </div>
         </div>
     );
 }
